a7919104b8557e45d9ea76c782a4818c1935f8b6
destrofik <92035823+destrofik@users.noreply.github.com>
2024-09-19 20:04:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+
+import asyncpg
+
+from app.handlers import router
+
+
+async def main():
+    load_dotenv()
+    bot = Bot(token=os.getenv('TG_TOKEN'))
+
+    conn = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
+    # Execute a statement to create a new table.
+    await conn.execute('''
+            CREATE TABLE test1(
+                id serial PRIMARY KEY,
+                name text,
+                dob date
+            )
+        ''')
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
