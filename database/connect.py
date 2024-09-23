2b2f9e3790c2bd344b176f9c49768df60ba45a95
destrofik <92035823+destrofik@users.noreply.github.com>
2024-09-24 00:08:00 +0300

    Add files via upload
    Signed-off-by: destrofik <92035823+destrofik@users.noreply.github.com>

--- database/connect.py
+++ database/connect.py
@@ -3,25 +3,28 @@ import asyncpg
 import aiopg
 import psycopg2
 from aiogram import types
-from aiogram.types import Message
+
 from dotenv import load_dotenv
+from aiogram.fsm.context import FSMContext
+
 
 load_dotenv()
-
 
 async def db_connect():
     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
 
 
-
-#Execute a statement to create a new table.
-# await connection.execute('''
-#         CREATE TABLE test1(
-#             id serial PRIMARY KEY,
-#             name text,
-#             dob date
-#             )''')
-
 async def fio_insert(chat_id, name):
     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
-    await connection.execute("INSERT INTO users_register (tg_id, fio) VALUES ($1, $2)",  chat_id, str(name))
+    async with connection.transaction():
+        await connection.execute("INSERT INTO users_register (tg_id, fio) VALUES ($1, $2)",  chat_id, str(name))
+    connection.close()
+
+async def is_user_in_databse(user_id):
+    connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
+    exists = await connection.fetchval("SELECT 1 FROM users_register WHERE tg_id = $1", user_id)
+
+    return exists
+
+
+
