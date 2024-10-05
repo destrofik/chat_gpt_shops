999e5630ba303cd8c1a2296fe1cf24803eb0055a
destrofik <92035823+destrofik@users.noreply.github.com>
2024-10-06 01:17:15 +0300

    Add files via upload
    Signed-off-by: destrofik <92035823+destrofik@users.noreply.github.com>

--- database/connect.py
+++ database/connect.py
@@ -20,11 +20,36 @@ async def fio_insert(user_telegram_id: int, name):
         await connection.execute("INSERT INTO users_register (tg_id, fio) VALUES ($1, $2)",  user_telegram_id, str(name))
     connection.close()
 
-async def is_user_in_databse(user_id):
+async def is_user_in_database(user_id):
     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
     exists = await connection.fetchval("SELECT 1 FROM users_register WHERE tg_id = $1", user_id)
 
     return exists
 
+async def view_user_profile(user_telegram_id: int):
+    connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
+    user_profile = await connection.fetchrow(
+        '''SELECT 
+            tg_id, 
+            fio, 
+            TO_CHAR(registration_date, 'FMMonth DD, YYYY') AS registration_date 
+        FROM users_register 
+        WHERE tg_id = $1''', user_telegram_id)
+    await connection.close()
 
+    return user_profile
+
+
+async def edit_fio_profile(user_telegram_id: int, name):
+    connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
+    async with connection.transaction():
+        await connection.execute("UPDATE users_register SET fio = $2 WHERE tg_id = $1",  user_telegram_id, str(name))
+    await connection.close()
+
+
+# async def edit_address_profile(user_telegram_id: int):
+#     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
+#     async with connection.transaction():
+#     await connection.execute("UPDATE users_register SET address = $1",  str(address))
+#     await connection.close()
 
