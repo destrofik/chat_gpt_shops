bd557f98a90eb12bb0a88481e28e817c391e0bd2
destrofik <92035823+destrofik@users.noreply.github.com>
2024-10-13 14:43:31 +0300

    Add files via upload
    Signed-off-by: destrofik <92035823+destrofik@users.noreply.github.com>

--- database/connect.py
+++ database/connect.py
@@ -1,11 +1,7 @@
 import os
 import asyncpg
-import aiopg
-import psycopg2
-from aiogram import types
 
 from dotenv import load_dotenv
-from aiogram.fsm.context import FSMContext
 
 
 load_dotenv()
@@ -17,14 +13,16 @@ async def db_connect():
 async def fio_insert(user_telegram_id: int, name):
     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
     async with connection.transaction():
-        await connection.execute("INSERT INTO users_register (tg_id, fio) VALUES ($1, $2)",  user_telegram_id, str(name))
+        await connection.execute("INSERT INTO users_register (tg_id, fio) VALUES ($1, $2)", user_telegram_id, str(name))
     connection.close()
+
 
 async def is_user_in_database(user_id):
     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
     exists = await connection.fetchval("SELECT 1 FROM users_register WHERE tg_id = $1", user_id)
 
     return exists
+
 
 async def view_user_profile(user_telegram_id: int):
     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
@@ -43,8 +41,9 @@ async def view_user_profile(user_telegram_id: int):
 async def edit_fio_profile(user_telegram_id: int, name):
     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
     async with connection.transaction():
-        await connection.execute("UPDATE users_register SET fio = $2 WHERE tg_id = $1",  user_telegram_id, str(name))
+        await connection.execute("UPDATE users_register SET fio = $2 WHERE tg_id = $1", user_telegram_id, str(name))
     await connection.close()
+
 
 async def get_products(page: int, limit: int):
     offset = (page - 1) * limit
@@ -56,9 +55,10 @@ async def get_products(page: int, limit: int):
         ORDER BY product_id 
         LIMIT {limit} OFFSET {offset}
     """)
-    
+
     await connection.close()
     return products
+
 
 async def get_total_products():
     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
@@ -66,8 +66,34 @@ async def get_total_products():
     await connection.close()
     return total
 
+
 # async def edit_address_profile(user_telegram_id: int):
 #     connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
 #     async with connection.transaction():
 #     await connection.execute("UPDATE users_register SET address = $1",  str(address))
 #     await connection.close()
+
+async def get_product_from_db(product_name):
+    """
+    Извлекает информацию о продукте (цену и описание) из базы данных PostgreSQL.
+
+    Args:
+    - product_name: Название продукта (например, "iPhone 15").
+
+    Returns:
+    - Словарь с ценой и описанием продукта или сообщение об ошибке.
+    """
+    connection = await asyncpg.connect(os.getenv('POSTGRESQL_URL'))
+
+    try:
+        # Выполнение запроса к базе данных
+        query = "SELECT price, description FROM products WHERE LOWER(name) = LOWER($1)"
+        product_data = await connection.fetchrow(query, product_name)
+
+        if product_data:
+            return {"price": product_data['price'], "description": product_data['description']}
+        else:
+            #return {"price": ['Нет в наличии'], "description": product_data['description']}
+            return {"error": f"Продукт <strong>{product_name}</strong> не найден в базе данных."}
+    finally:
+        await connection.close()
